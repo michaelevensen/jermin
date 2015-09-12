1894010be9c1718953d14370dd4a7e945d40040b
Michael Nino Evensen <michaelevensen@gmail.com>
2015-09-12 20:02:05 +0200

    simple client-side duplicate check implemented

--- client/templates/item/new.js
+++ client/templates/item/new.js
@@ -13,25 +13,51 @@ Template.new.events({
       url: $('input[name=new-post]').val()
     };
 
-    // add
-    Meteor.call('addPost', post, function(error, result) {
-      if(error) {
-        return alert(error.reason);
+    /*
+    * Validate duplicates
+    */
+    var errors = {};
+
+    // Groups
+    if(_.isUndefined(template.data.username)) {
+      var groupId = template.data._id;
+      if(Posts.findOne({url: post.url, groupIds: {$in: [groupId]}})) {
+        errors.exists = 'This post already exists in this group.';
       }
+    }
+    // Profiles
+    else {
+      var userId = Meteor.userId();
+      if(Posts.findOne({authorId: userId, url: post.url, groupIds: {$exists: false}})) {
+        errors.exists = 'This post already exists on your profile.';
+      }
+    }
 
-      if(result) {
-        // For groups
-        if(_.isUndefined(template.data.username)) {
-          var postId = result;
-          var groupId = template.data._id;
-
-          // add post to group
-          Meteor.call('addPostToGroup', postId, groupId);
+    //
+    if(!_.isEmpty(errors)) {
+      return alert(errors.exists);
+    }
+    else {
+      // add
+      Meteor.call('addPost', post, function(error, result) {
+        if(error) {
+          return alert(error.reason);
         }
-      }
-    });
 
-    // reset input
-    $('input[name=new-post]').val('');
+        if(result) {
+          // For groups
+          if(_.isUndefined(template.data.username)) {
+            var postId = result;
+            var groupId = template.data._id;
+
+            // add post to group
+            Meteor.call('addPostToGroup', postId, groupId);
+          }
+        }
+      });
+
+      // reset input
+      $('input[name=new-post]').val('');
+    }
   },
 });
